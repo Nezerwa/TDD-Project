7296929c10f9bc4fd1e6e1ece7b9348a3fc091c7
Lugard Agu <agulugard@gmail.com>
2023-05-05 09:32:32 +0100

    Update reverse_spec file

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -1,2 +1,10 @@
 require_relative('../solver')
 
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+  it 'returns word reversed' do
+    expect(@solver.reverse('hello')).to eql('olleh')
+  end
+end
